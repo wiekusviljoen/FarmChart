1bb5c617f11e351d3afb3b63cd396db0fc3dd3d7
wiekusviljoen <wiekusviljoen@gmail.com>
2023-02-18 00:38:06 +0200

    commit on master

---
+++ Farm/Controllers/BullController.cs
@@ -0,0 +1,42 @@
+﻿using Farm.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Farm.Controllers
+{
+    public class BullController : Controller
+    {
+
+        private readonly FarmContext _context;
+
+        public BullController(FarmContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowCattle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public List<object> GetCattle()
+        {
+            List<object> data = new List<object>();
+
+            List<string> labels = _context.Cattle.Select(p => p.Camp).ToList();
+            data.Add(labels);
+
+            List<int> SalesNumber = _context.Cattle.Select(p => p.Bulls).ToList();
+
+            data.Add(SalesNumber);
+
+            return data;
+        }
+    }
+}
