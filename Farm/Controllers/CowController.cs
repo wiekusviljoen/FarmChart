47bfdd356b5da36ca9db7030fdb19d83b9424c89
wiekusviljoen <wiekusviljoen@gmail.com>
2023-02-20 19:34:18 +0200

    commit on master

---
+++ Farm/Controllers/CowController.cs
@@ -0,0 +1,42 @@
+﻿using Farm.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Farm.Controllers
+{
+    public class CowController : Controller
+    {
+        private readonly FarmContext _context;
+
+        public CowController(FarmContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowCattle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public List<object> GetCattleCow()
+        {
+            List<object> data = new List<object>();
+
+            List<string> labels = _context.Cattle.Select(p => p.Camp).ToList();
+            data.Add(labels);
+
+            List<int> SalesNumber = _context.Cattle.Select(p => p.Cows).ToList();
+
+            data.Add(SalesNumber);
+
+            return data;
+        }
+        
+
+    }
+}
