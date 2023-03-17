2bba76acb591a63d18fe6a457b5ffaac20e40c0d
wiekusviljoen <wiekusviljoen@gmail.com>
2023-03-17 13:45:04 +0200

    commit on master

--- Farm/Controllers/HomeController.cs
+++ Farm/Controllers/HomeController.cs
@@ -6,12 +6,9 @@ namespace Farm.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        
 
         public HomeController()
         {
